1c6936d870953419c00ca2db90a2e53b349bcc92
Anton Karpenko <antykkarp@gmail.com>
2019-03-18 22:33:00 +0200

    Add Image model with compression

--- config/settings/base.py
+++ config/settings/base.py
@@ -74,7 +74,8 @@ THIRD_PARTY_APPS = [
     'rest_auth',
 ]
 LOCAL_APPS = [
-    'apps.users.apps.UsersAppConfig'
+    'apps.users.apps.UsersAppConfig',
+    'apps.images.apps.ImagesAppConfig',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
